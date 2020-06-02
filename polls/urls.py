6138a6f90b0f6d858e45b1129f33c717dc3a418e
Rita <anaritacruzgomes@gmail.com>
2020-06-02 21:05:20 +0200

    added generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,14 +4,9 @@ from . import views
 
 app_name = 'polls'
 
-# Generic views - index, detail and results are similar
 urlpatterns = [
-    # index
     path('', views.IndexView.as_view(), name='index'),
-    # detail
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # results
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # question id
+    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
